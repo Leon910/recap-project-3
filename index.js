e104e68555ee9241adb48f68c83f1c224610da17
Predrag <predrag.babic@tactix-sports.com>
2024-07-15 12:08:36 +0200

    create fetch data function

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    cardContainer.innerHTML = "";
+    data.results.forEach((character) => {
+      const cardElement = CharacterCard(character);
+      cardContainer.append(cardElement);
+    });
+  } catch (error) {
+    console.error("Could not fetch data", error);
+  }
+}
+fetchCharacters();
